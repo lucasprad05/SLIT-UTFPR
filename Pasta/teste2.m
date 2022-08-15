851978571ef2491434f4d4b63416b85cf4bd6bc0
Lucas Prado <88468707+lucasprad05@users.noreply.github.com>
2022-08-15 16:00:46 -0300

    Add files via upload

---
+++ Pasta/teste2.m
@@ -0,0 +1,29 @@
+%% Clean Base
+
+clear all
+close all
+clc
+
+%% Code
+t = 0:0.01:2;
+y = -3:0.01:3;
+
+x = sin(2*pi*t);
+plot(t, x, 'k--');
+
+hold all
+
+x = 2*sin(2*pi*t);
+plot(t, x, 'r');
+
+x = 0.5*sin(2*pi*t);
+plot(t, x, 'b');
+
+title('Mudança de escala: y=a*x');
+ylabel('Amplitude')
+xlabel('Tempo (s)')
+
+legend({'x(t) = sen(2\pit)', 'x(t) = 2sen(2\pit)', 'x(t) = 0.5sen(2\pit)'});
+
+ylim([-3 3])
+grid on
